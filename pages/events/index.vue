4350da636619cfdb848bb0f75b57f6617f42e516
mikelml <cesar.segovia@g-tec.io>
2019-01-03 13:33:28 -0600

    03/01/18

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -9,16 +9,6 @@ import components from '../../.nuxt/components/nuxt-link';
     </v-flex>
     <v-flex>
       <v-layout align-center justify-center row>
-        <v-flex xs3>
-          <v-container>
-            <v-btn block round color="info" @click="log_data()">create</v-btn>
-          </v-container>
-        </v-flex>
-        <v-flex xs3>
-          <v-container>
-            <v-btn block round color="success">management</v-btn>
-          </v-container>
-        </v-flex>
         <v-flex>
             <v-layout align-center justify-center row>
                 <v-flex xs3> <v-container><v-btn block round color="info" to="/events/formEvents"> create</v-btn></v-container></v-flex>                
@@ -35,29 +25,30 @@ import grid from "~/components/grid.vue";
 export default {
   data() {
     return {
-      img_link:
-        "https://images.unsplash.com/photo-1503293050619-6048ffad0dc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80",
-      api_url:
-        "https://ox8usqk4cd.execute-api.us-east-2.amazonaws.com/hackathon/events",
-      data: {},
-      grid_props: {
-        headers: [
-          { text: "Event Name", value: "event_name" },
-          { text: "Place", value: "place" },
-          { text: "Location", value: "location" },
-          { text: "Time event", value: "time_event" }
-        ],
-        items: [
-          {
-            event_name: "Event 1",
-            place: "Stadium one",
-            location: "one street",
-            time_event: "17:00hr",
-            address: {
-              city: "asd"
+        img_link:
+            "https://images.unsplash.com/photo-1503293050619-6048ffad0dc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80",
+        api_url:
+            "https://ox8usqk4cd.execute-api.us-east-2.amazonaws.com/hackathon/events",
+        data: {},
+        grid_props: {
+            headers: [
+            { text: "Event Name", value: "event_name" },
+            { text: "Place", value: "place" },
+            { text: "Location", value: "location" },
+            { text: "Time event", value: "time_event" }
+            ],
+            items: [
+                {
+                    event_name: "Event 1",
+                    place: "Stadium one",
+                    location: "one street",
+                    time_event: "17:00hr",
+                    address: {
+                    city: "asd"
+                        }
+                    }
+                ]
             }
-<<<<<<< HEAD
-        
         }
     },
     components: {
@@ -70,25 +61,6 @@ export default {
         logdata(){
             console.log(this.grid_props.items)
         }
-=======
-          }
-        ]
-      }
-    };
-  },
-  components: {
-    grid
-  },
-  mounted() {
-    this.$axios.$get(this.api_url, { progress: false }).then(res => {
-      this.grid_props.items = res;
-    });
-  },
-  methods: {
-    log_data() {
-      console.log(this.grid_props.items);
->>>>>>> 3ee420c9afcefc587a8776fc02ef7f66e2a00a17
     }
-  }
 };
 </script>
